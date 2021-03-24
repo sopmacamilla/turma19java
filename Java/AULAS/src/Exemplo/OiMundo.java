df8153c96bd64adedf817af2f77f1fb68c176201
Camilla C <camillaoui@gmai.com>
2021-03-24 16:45:24 -0300

    Primeiro commit turma19java em Java

---
+++ Java/AULAS/src/Exemplo/OiMundo.java
@@ -0,0 +1,12 @@
+package Exemplo;
+
+public class OiMundo {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Oi turma 19 Generation!");
+		System.out.println("Vamos progamar em Java!");
+		
+	}
+
+}
